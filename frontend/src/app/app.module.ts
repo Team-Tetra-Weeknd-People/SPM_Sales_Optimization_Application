3bde949aca2e6ed22bd53c3863493d3b0859bc7f
Randula Mawaththta <randula98@gmail.com>
2023-09-03 23:57:32 +0530

    Font awesome Added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -35,6 +36,7 @@ import { RouteComponent } from './pages/route/route.component';
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
