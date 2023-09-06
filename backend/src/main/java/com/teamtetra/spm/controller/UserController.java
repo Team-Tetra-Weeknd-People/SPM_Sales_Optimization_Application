8c8a6a64b38f0f485465ea55170df4836af64a66
Randula Mawaththta <randula98@gmail.com>
2023-09-06 21:36:49 +0530

    User API

--- backend/src/main/java/com/teamtetra/spm/controller/UserController.java
+++ backend/src/main/java/com/teamtetra/spm/controller/UserController.java
@@ -1,2 +1,76 @@
-package com.teamtetra.spm.controller;public class UserController {
+package com.teamtetra.spm.controller;
+
+import com.teamtetra.spm.model.Test;
+import com.teamtetra.spm.model.User;
+import com.teamtetra.spm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(value = "*")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/")
+    public ResponseEntity<User> save(@RequestBody User body){
+        User responseBody = service.save(body);
+
+        if(responseBody == body) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new User());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable String id , @RequestBody User body){
+        boolean response = service.update(id , body);
+
+        if(response) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(body);
+        }else{
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(body);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable String id){
+        boolean response = service.delete(id);
+
+        if(response){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(new User());
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new User());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> getAll(){
+        List<User> dataSet = service.getAll();
+
+        if(!dataSet.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(dataSet);
+        }else{
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<User>> getOne(@PathVariable String id){
+        Optional<User> data = service.getOne(id);
+
+        if(data.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(data);
+        }else{
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+    }
 }
--- backend/src/main/java/com/teamtetra/spm/model/User.java
+++ backend/src/main/java/com/teamtetra/spm/model/User.java
@@ -1,2 +1,29 @@
-package com.teamtetra.spm.model;public class User {
+package com.teamtetra.spm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Document(collection = "User")
+public class User {
+
+    @Id
+    private String id;
+    private String fName;
+    private String lName;
+
+    @Indexed(unique = true)
+    private String email;
+
+    @Indexed(unique = true)
+    private String contactNo;
+    private String image;
+    private String password;
 }
