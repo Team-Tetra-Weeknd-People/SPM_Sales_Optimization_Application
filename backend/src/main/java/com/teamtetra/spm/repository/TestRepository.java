3e7c3911d4561367e62cee89b70ff49210f9a2b8
Randula Mawaththta <randula98@gmail.com>
2023-09-07 00:55:59 +0530

    remove sample apis

--- backend/src/main/java/com/teamtetra/spm/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teamtetra.spm.repository;
-
-import com.teamtetra.spm.model.Test;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepository extends MongoRepository<Test, String> {
-}
