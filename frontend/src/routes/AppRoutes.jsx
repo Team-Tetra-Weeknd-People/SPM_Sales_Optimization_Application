6fb60993de3b43a0fa428b0301ddc0f24672885c
Randula Mawaththta <randula98@gmail.com>
2023-09-08 02:02:28 +0530

    Item add, View, Barcode creation Done

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from '../pages/home';
 import Login from '../pages/authentication/login';
 import Register from '../pages/authentication/register';
 import ItemsMain from '../pages/items/items-main';
+import ItemAdd from '../pages/items/item-add';
+import BarcodeGenerator from '../pages/items/barcode-generator';
 
 export default function App() {
     return (
@@ -14,6 +16,8 @@ export default function App() {
                     <Route path="/register" element={<Register />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/items-main" element={<ItemsMain />} />
+                    <Route path="/item-add" element={<ItemAdd />} />
+                    <Route path="/barcode-generator/:itemID" element={<BarcodeGenerator />} />
                 </Routes>
             </Router>
         </>
