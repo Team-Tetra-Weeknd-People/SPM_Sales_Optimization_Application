1057abbb116748cad2fb6dc4d1048142fb8064bb
SuDelk <sudul.fernando@gmail.com>
2023-10-20 22:12:42 +0530

    graph

--- frontend/src/components/graph.jsx
+++ frontend/src/components/graph.jsx
@@ -7,47 +7,27 @@ import {
   CartesianGrid,
   Legend,
   ResponsiveContainer,
-  Customized,
-  Rectangle,
+  Tooltip,
 } from 'recharts';
-
-
-
-// using Customized gives you access to all relevant chart props
-const CustomizedRectangle = (props) => {
-  const { formattedGraphicalItems } = props;
-  // get first and second series in chart
-  const firstSeries = formattedGraphicalItems[0];
-
-  // render custom content using points from the graph
-  return firstSeries?.props?.points.map((firstSeriesPoint,) => {
-    return (
-      <Rectangle
-        key={firstSeriesPoint.payload.name}
-      />
-    )
-  })
-};
 
 const Graph = (props) => {
   const data = [
     {
-      name: props.cost,
-      Profit: 0,
+      Marked_Price: props.cost,
+      Profit_Percentage: 0,
     },
     {
-      name: props.msrp,
-      Profit: 100,
+      Marked_Price: props.msrp,
+      Profit_Percentage: 100,
     },
     {
-      name: props.level3,
-      Profit: 5,
+      Marked_Price: props.level3,
+      Profit_Percentage: 5,
     },
     {
-      name: props.level4,
-      Profit: 0,
+      Marked_Price: props.level4,
+      Profit_Percentage: 0,
     },
-    
   ];
 
   return (
@@ -63,12 +43,16 @@ const Graph = (props) => {
           bottom: 5,
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis hide={true}/>
-        <Legend />
-        <Line type="monotone" dataKey="Profit" stroke="#568463" />
-        <Customized component={CustomizedRectangle} />
+        <CartesianGrid strokeDasharray="9 9" />
+        <XAxis dataKey="Marked_Price" />
+        <YAxis hide={true} />
+        <Legend name='Max Profit Percentage' />
+        <Tooltip
+          formatter={(value, name) => {
+            return name === 'Marked_Price' ? `$${value.toFixed(2)}` : `${value.toFixed(2)}%`;
+          }}
+        />
+        <Line type="monotone" dataKey="Profit_Percentage" name='Max Profit - Percentage' stroke="#55ff55" />
       </LineChart>
     </ResponsiveContainer>
   );
