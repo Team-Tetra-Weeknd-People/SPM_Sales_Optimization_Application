d2196f62d511b83291fba2c723c38fdbaee7e49a
IT21008382 <it21008382@my.sliit.lk>
2023-10-20 18:22:03 +0530

    page updated

--- frontend/src/pages/Cashier/cashier-main.jsx
+++ frontend/src/pages/Cashier/cashier-main.jsx
@@ -4,21 +4,36 @@ import Select from "react-select";
 import Button from 'react-bootstrap/Button';
 import Table from "react-bootstrap/Table";
 import Swal from "sweetalert2";
+import Modal from "react-bootstrap/Modal";
+import { Link } from "react-router-dom";
 
 import "../../styles/madusha/cashier-main.css";
 import Navbar from "../../components/navbar";
-import { OrderedItemAdd } from "../../components/ordereditem-add";
+// import { OrderedItemAdd } from "../../components/ordereditem-add";
+import SalesSummaryReport from "./sales-summary-report";
 
 const CashierMain = () => {
   const [selectedItem, setSelectedItem] = useState([]);
   const [orderedItems, setOrderedItems] = useState([]);
   const [allItems, setAllItems] = useState([]);
+  const [allOrders, setAllOrders] = useState([]);
   const [ options, setOptions ] = useState(null);
   const [item, setItem] = useState([]);
+  const [show, setShow] = useState(false);
+  const [specificOrders, setSpecificOrders] = useState([]);
+  const [orderOptions, setOrderOptions] = useState([]);
+  let orderId = null;  
 
   const [discounts, setDiscounts] = useState([]);
   const [showDialog, setShowDialog] = useState(false);
   const [discountIndex, setDiscountIndex] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleView = () => {
+    handleShow();
+  };
 
   const handleOpenDialog = (index) => {
     setDiscountIndex(index); // Set the index for the discount to be updated
@@ -78,6 +93,87 @@ const handleUpdateDiscount = () => {
     }
   };
 
+  useEffect(() => {
+    getAllOrders();
+  }, []);
+
+  const getAllOrders = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/ordereditem/`
+      );
+      setAllOrders(response.data);
+      console.log('all orders: '+ allOrders);
+
+      allOrders.forEach((order) => {
+        if (order.orderId !== specificOrders.orderId) {
+          setSpecificOrders((prevOrders) => [...prevOrders, order]);
+        }
+      });
+      console.log('specific orders: '+ specificOrders);
+
+      // const orderOption = specificOrders.map((order) => ({
+      //   value: order.orderId,
+      //   label: order.orderId,
+      //   }));
+      //   setOrderOptions(orderOption);
+      //   console.log("Options " + orderOptions);
+
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    }
+  };
+
+  useEffect(() => {
+    // This will execute whenever specificOrders changes
+    const orderOption = specificOrders.map((order) => ({
+      value: order.orderId,
+      label: order.orderId,
+    }));
+    setOrderOptions(orderOption);
+    console.log("orderOptions " + orderOptions);
+  }, [specificOrders]);
+
+  async function addOrderedItem() {
+    //upload image to firebase
+    const values = [...orderedItems];
+
+    if (allOrders.length === 0) {
+      orderId = 1000;
+    } else {
+      allOrders.forEach((order) => {
+        if (order.id > orderId) {
+          orderId = order.id;
+          console.log('order id: ' + orderId);
+        }
+      });
+    }
+
+    values.forEach(async (item, index) => {
+        const data = {
+            orderId: orderId + 1,
+            itemCode: item.itemCode,
+            name: item.name,
+            barcode: item.barcode,
+            description: item.description,
+            orderedquantity: 1,
+            brand: item.brand,
+            retailPrice: item.retailPrice,
+            discountPrice: item.retailPrice*(discounts[index]/100) || 0,
+            sellingPrice: item.retailPrice - item.retailPrice*(discounts[index]/100) ||  item.retailPrice,
+        };
+        await axios
+            .post(`${import.meta.env.VITE_BACKEND_URL}/ordereditem/`, data)
+            .then((res) => {
+            console.log(res.data);
+            sessionStorage.setItem("itemJustAdded", data);
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        });
+  }
+
   async function updateItems(orderedItems) {
     //upload image to firebase
     const values = [...orderedItems];
@@ -119,13 +215,12 @@ const handleUpdateDiscount = () => {
         confirmButtonText: 'Yes',
         denyButtonText: `No`,
       }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-            OrderedItemAdd(orderedItems, discounts);
+            addOrderedItem();
             setSelectedItem(null);
             window.location.reload();
             updateItems(orderedItems);
-          Swal.fire('Purchased!', '', 'success')
+            Swal.fire('Purchased!', '', 'success')
         } else if (result.isDenied) {
           Swal.fire('Purchase Denied', '', 'info')
         }
@@ -135,6 +230,34 @@ const handleUpdateDiscount = () => {
 
   return (
     <>
+    <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Select Order to Generate Report</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="select-order-no-div">
+            <Select
+              options={orderOptions} 
+              onChange={handleSelectItem}
+              value={selectedItem}
+            />
+          </div>   
+        </Modal.Body>
+        <Modal.Footer>
+        <Button variant="primary" onClick={handleClose}>
+            Select Order
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     <div>
       <Navbar />
       <h1 className="cashier-main-title">Cashier</h1>
@@ -164,7 +287,7 @@ const handleUpdateDiscount = () => {
         </div>
       )}
       </div>
-      <div className="table">
+      <div className="order-table">
         <h2>Ordered Items</h2>
         {orderedItems && (
         <Table striped bordered hover>
@@ -192,7 +315,7 @@ const handleUpdateDiscount = () => {
                   <td>1</td>
                   <td>${sitem.retailPrice}</td>
                   <td>{discounts[index] || 0}%</td>
-                  <td>${(sitem.retailPrice - sitem.retailPrice*(discounts[index]/100)) ||  sitem.retailPrice}</td>
+                  <td>${(sitem.retailPrice - sitem.retailPrice*(discounts[index]/100).toFixed(2)) ||  sitem.retailPrice}</td>
                   <td><Button variant="warning" onClick={() => handleOpenDialog(index)}>Set Discount</Button>{' '}</td>
                   <td><Button variant="warning" onClick={() => removeItem(sitem.id)}>Remove Item</Button>{' '}</td>
               </tr>
@@ -204,14 +327,20 @@ const handleUpdateDiscount = () => {
             <div className="showdialog-container">
             <h3>Update Discount Price</h3>
             <input
-                type="number"
-                placeholder="New Discount Price"
-                value={discounts[discountIndex] || ''}
-                onChange={(e) => setDiscounts((prevDiscounts) => {
+              type="number"
+              placeholder="New Discount Price"
+              max="100"
+              value={discounts[discountIndex] || ''}
+              onChange={(e) => {
+                const inputValue = e.target.value;
+                if (/^\d*$/.test(inputValue) && inputValue >= 1 && inputValue <= 100) {
+                  setDiscounts((prevDiscounts) => {
                     const updatedDiscounts = [...prevDiscounts];
-                    updatedDiscounts[discountIndex] = e.target.value;
+                    updatedDiscounts[discountIndex] = inputValue;
                     return updatedDiscounts;
-                  })}
+                  });
+                }
+              }}
             />
             <button className="dialog-btn" onClick={handleUpdateDiscount}>Save</button>
             <button className="dialog-btn" onClick={handleCloseDialog}>Cancel</button>
@@ -220,6 +349,13 @@ const handleUpdateDiscount = () => {
             {orderedItems.length !== 0 && (
               <button onClick={() => PurchaseOrder()} >Purchase Order</button>
             )}
+            <div className="sales-summary-report-button-container">
+              <Link to="/sales-summary-report">
+              <button class="sales-summary-report-button" >Sales Summary Report</button>
+              </Link>
+          </div>
+      </div>
+      <div>
       </div>
     </div>
     </>
